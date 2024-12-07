4956d5b6f64aecf7af50b8cb25106f40276a9622
David Paparo <d.paparo@tiscali.it>
2024-12-07 12:02:44 +0100

    carico esercizio completo

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 
 
-let saluto = `ciao`
+let base = 10;
 
-console.log(saluto);
+let altezza = 20;
+
+let somma = base + altezza;
+
+let perimetro = somma * 2;
+
+let diff = altezza - base;
+
+let area_triang = base * altezza / 2;
+
+let quadrato = base**2;
+
+console.log(quadrato);
+
+
+console.log( `Il lotto ha una forma rettangolare con una lunghezza pari a ml ${base}, una larghezza pari a ml ${altezza}`);
+
+console.log( `La superficie totale è pari a mq ${base * altezza}` );
+
+console.log( `Il perimetro è pari a ml ${perimetro}` );
+
+console.log( `La differenza tra lato corto e lungo è pari a ml ${diff} ` );
+
+console.log( `L'area del triangolo iscritto al suo interno è pari a mq ${area_triang}` );
+
+console.log( `Il quadrato del lato corto è pari a mq ${quadrato}` );
+
+
+
+
